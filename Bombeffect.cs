9291f37f8240862130cfc3d04d7cc1099dd1649e
チョク　スンシュン <xiong9605@gmail.com>
2019-11-11 09:48:44 +0900

    Add files via upload

---
+++ Bombeffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombeffect : MonoBehaviour
+{
+    public GameObject player;
+    public void BombShoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision player)
+     {
+        if (player.gameObject.CompareTag("bomb"))
+        {
+            BombShoot(new Vector3(0, 200, 200));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
