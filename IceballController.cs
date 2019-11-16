b638c12cc77e8b34926e24e90d20f3221176a719
チョク　スンシュン <xiong9605@gmail.com>
2019-11-16 09:28:44 +0900

    Add files via upload

---
+++ IceballController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceballController : MonoBehaviour
+{
+   
+
+    public GameObject iceball;
+    public void ice(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+        transform.parent = GameObject.Find("target").transform;　//プレイヤーのchildrenになる
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        Destroy(gameObject,5f);       
+    }
+}
