3f1eee62b4611735491ddbb5d048a57b305e8aaa
xiaohaotin <>
2019-12-14 18:36:13 +0900

    follow camera script　を修正しました。

--- Assets/XiaoHaoTing/Scripts/ADV_FollowCamera.cs
+++ Assets/XiaoHaoTing/Scripts/ADV_FollowCamera.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ADV_FollowCamera : MonoBehaviour
 {
+    //public Transform playerTransform;
+    //public Vector3 deviation;
+
+    //void Start()
+    //{
+    //    deviation = transform.position - playerTransform.position;
+    //}
+
+    //private void Update()
+    //{
+    //    transform.position = playerTransform.position + deviation;
+    //}
+
+
 
     public GameObject player;   //プレイヤー情報格納用
-    public Vector3 offset;      //相対距離取得用
-
+    Vector3 offset;      //相対距離取得用
+    public float smoothing = 5f;
     // Use this for initialization
     void Start()
     {
 
-        //unitychanの情報を取得
+        //playerの情報を取得
         this.player = GameObject.Find("Player");
 
         // MainCamera(自分自身)とplayerとの相対距離を求める
@@ -21,14 +35,14 @@ public class ADV_FollowCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         //新しいトランスフォームの値を代入する
         transform.position = player.transform.position + offset;
 
         //playerの向きと同じようにカメラの向きを変更する
-        transform.rotation = player.transform.rotation;
-
+        //transform.rotation = player.transform.rotation;
+        transform.position = Vector3.Lerp(transform.position, player.transform.position,smoothing * Time.deltaTime);
     }
 }
