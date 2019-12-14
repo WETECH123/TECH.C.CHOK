04cb6b9c286f07a6662f11a45cb0df7aa9f76fa0
xiaohaotin <>
2019-12-14 16:26:33 +0900

    xiaohaoting branchを　追加します

---
+++ Assets/XiaoHaoTing/Scripts/ADV_Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADV_Player : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+     void Start()
+    {
+        target = ADV_Waypoints.points[0];        
+    }
+
+     void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position,target.position)<= 0.2f)
+        {
+            GetNexWaypoint();
+        }
+    }
+
+    void GetNexWaypoint()
+    {
+        if (wavepointIndex >= ADV_Waypoints.points.Length -1)
+        {
+            //Destroy(gameObject);
+        }
+        wavepointIndex++;
+        //target = ADV_Waypoints.points[wavepointIndex];
+    }
+   
+}
